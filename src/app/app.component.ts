f4ec25f4bd21ccae9ed582c1f6642fcf43a3d1bd
Derek Krebs <derekkrebs@comcast.net>
2019-10-08 23:10:13 -0500

    Adding sorting of articles based on points

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,10 @@ export class AppComponent {
     title.value = '';
     link.value = '';
     return false;
-  }
+	}
+  
+	sortedArticles(): Article[] {
+		return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+	}
 	
 }
